9c72c641b283ce9aef91889078823cc29139f560
ISKondratyev <kotoezhikne@gmail.com>
2017-10-02 16:45:50 +0300

    Первичные превращения. Функция переводит чч:мм в римские цифры в диапазоне 00:00 - 23:59. Необходимо учесть проверку на тип ошибки

--- javascript-task-1/roman-time.js
+++ javascript-task-1/roman-time.js
@@ -4,9 +4,87 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+//var time;
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return time;
+	var timeHH = time.substr(0,2);
+	var timeMM = time.substr(3,2);
+	
+	if (time.length != 5 || timeHH < 0 || timeHH >= 24 || timeMM < 0 || timeMM > 59) {
+		time = 'Неверное значение';
+		return time;
+	}
+	
+	function convertNumber(baseNumber) {
+		if (baseNumber == 0) return 'N';
+		
+		var firstNumber = +baseNumber.substr(0,1);
+		var secondNumber = +baseNumber.substr(1,1);
+		
+		switch (firstNumber) {			
+			case 0:
+				firstNumber = '';
+				break;
+			case 1:
+				firstNumber = 'X';
+				break;
+			case 2:
+				firstNumber = 'XX';
+				break;
+			case 3:
+				firstNumber = 'XXX';
+				break;
+			case 4:
+				firstNumber = 'XL';
+				break;
+			case 5:
+				firstNumber = 'L';
+				break;
+		}
+		
+		switch (secondNumber) {
+			case 0:
+				secondNumber = '';
+				break;
+			case 1:
+				secondNumber = 'I';
+				break;
+			case 2:
+				secondNumber = 'II';
+				break;
+			case 3:
+				secondNumber = 'III';
+				break;
+			case 4:
+				secondNumber = 'IV';
+				break;
+			case 5:
+				secondNumber = 'V';
+				break;
+			case 6:
+				secondNumber = 'VI';
+				break;
+			case 7:
+				secondNumber = 'VII';
+				break;
+			case 8:
+				secondNumber = 'VIII';
+				break;
+			case 9:
+				secondNumber = 'IX';
+				break;
+		}
+		
+		
+		return firstNumber + secondNumber;
+	}
+	
+	//console.log(timeHH + ':' + convertNumber(timeMM));
+	//console.log(timeHH + ':' + timeMM);
+	
+    return time = convertNumber(timeHH) + ':' + convertNumber(timeMM);
 }
 
-module.exports = romanTime;
+console.info(romanTime('23:59'));
+
+//module.exports = romanTime;
